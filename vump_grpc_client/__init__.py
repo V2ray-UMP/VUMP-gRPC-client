6c7760131ee045cee46e5f7be0cddf6e50f653ac
Mahdi Zojaji <mmahdizojajim@gmail.com>
2022-11-16 21:28:46 +0330

    First package of vump

---
+++ vump_grpc_client/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.0.0"
+__license__ = "Massachusetts Institute of Technology (MIT)"
+__copyright__ = "Copyright (C) 2022-present V2ray-UMP <https://github.com/V2ray-UMP>"
+
+from . import exceptions
+from .ext import utils
+from .client import VUMPClient
